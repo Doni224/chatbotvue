ec51da15f7a637daa272a90c311619a9dca89ab5
Doni224 <zizam2340@gmail.com>
2023-10-28 15:20:29 +0700

    chatbotvue3

--- src/components/SemuaKoleksi.vue
+++ src/components/SemuaKoleksi.vue
@@ -81,7 +81,16 @@
 </div>
     </div>
     
-    
+    <div class="col-lg-4 py-4">
+        <div class="card" style="width: 18rem;">
+  <img src="../assets/img/alat batu.jpg" class="card-img-top img-inline" alt="...">
+  <div class="card-body">
+    <h4 class="card-title">Alat Batu</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Lihat Selengkapnya</a>
+  </div>
+</div>
+    </div>
    
     
 </div>
